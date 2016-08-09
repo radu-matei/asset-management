f567743e62d92bea9661dcc2beb32ea05df3033d
Radu Matei <matei.radu94@gmail.com>
2016-08-09 16:47:03 +0300

    Added EF database context class

---
+++ AssetManagement/AssetManagement.Common/AssetManagementContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace AssetManagement.Common
+{
+    public class AssetManagementContext : DbContext
+    {
+        public AssetManagementContext() : base("name=AssetManagementContext")
+        {
+        }
+
+        public DbSet<AssetType> AssetTypes { get; set; }
+        public DbSet<Asset> Assets { get; set; }
+        public DbSet<Log> Logs { get; set; }
+    }
+}
