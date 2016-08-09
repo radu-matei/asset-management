7f15364b9758b63d36a6dadc607553c211251640
Radu Matei <matei.radu94@gmail.com>
2016-08-09 16:42:30 +0300

    Added Asset

---
+++ AssetManagement/AssetManagement.Common/Asset.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AssetManagement.Common
+{
+    public class Asset
+    {
+        public int Id { get; set; }
+
+        public int AssetTypeId { get; set; }
+
+        [DisplayName("Asset Name")]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public virtual AssetType AssetType { get; set; }
+        public ICollection<Log> Logs { get; set; }
+    }
+}
