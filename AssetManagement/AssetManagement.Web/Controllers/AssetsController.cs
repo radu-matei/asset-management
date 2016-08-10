483e4e525cf8c6d574b2143883a75facb47c85b7
Radu Matei <matei.radu94@gmail.com>
2016-08-10 13:43:43 +0300

    Added Assets controller

---
+++ AssetManagement/AssetManagement.Web/Controllers/AssetsController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AssetManagement.Common;
+
+namespace AssetManagement.Web.Controllers
+{
+    public class AssetsController : Controller
+    {
+        private AssetManagementContext db = new AssetManagementContext();
+
+        // GET: Assets
+        public ActionResult Index()
+        {
+            var assets = db.Assets.Include(a => a.AssetType);
+            return View(assets.ToList());
+        }
+
+        // GET: Assets/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asset asset = db.Assets.Find(id);
+            if (asset == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asset);
+        }
+
+        public ActionResult Logs(int? assetId)
+        {
+            if (assetId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var asset = db.Assets.Find(assetId);
+            if (asset == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.AssetName = asset.Name;
+            return View(asset.Logs);
+        }
+
+        // GET: Assets/Create
+        public ActionResult Create()
+        {
+            ViewBag.AssetTypeId = new SelectList(db.AssetTypes, "Id", "Name");
+            return View();
+        }
+
+        // POST: Assets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,AssetTypeId,Name")] Asset asset)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Assets.Add(asset);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AssetTypeId = new SelectList(db.AssetTypes, "Id", "Name", asset.AssetTypeId);
+            return View(asset);
+        }
+
+        // GET: Assets/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asset asset = db.Assets.Find(id);
+            if (asset == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AssetTypeId = new SelectList(db.AssetTypes, "Id", "Name", asset.AssetTypeId);
+            return View(asset);
+        }
+
+        // POST: Assets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,AssetTypeId,Name")] Asset asset)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(asset).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AssetTypeId = new SelectList(db.AssetTypes, "Id", "Name", asset.AssetTypeId);
+            return View(asset);
+        }
+
+        // GET: Assets/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asset asset = db.Assets.Find(id);
+            if (asset == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asset);
+        }
+
+        // POST: Assets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Asset asset = db.Assets.Find(id);
+            db.Assets.Remove(asset);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
