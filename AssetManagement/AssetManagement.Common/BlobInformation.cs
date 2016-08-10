5846d659384bb370af66b3e32584bc98aaa12c35
Radu Matei <matei.radu94@gmail.com>
2016-08-10 13:40:37 +0300

    Added BlobInformation class

---
+++ AssetManagement/AssetManagement.Common/BlobInformation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace AssetManagement.Common
+{
+    public class BlobInformation
+    {
+        public Uri BlobUri { get; set; }
+
+        public string BlobName
+        {
+            get
+            {
+                return BlobUri.Segments[BlobUri.Segments.Length - 1];
+            }
+        }
+        public string BlobNameWithoutExtension
+        {
+            get
+            {
+                return Path.GetFileNameWithoutExtension(BlobName);
+            }
+        }
+
+        public int LogId { get; set; }
+    }
+}
